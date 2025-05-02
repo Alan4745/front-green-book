f511c3efdfb2b5ed039550307ba52c0cc8ad06a0
Eduardo-45 <alaneduardodelacruz407@gmail.com>
2025-05-02 10:26:44 -0600

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div class="flex justify-center items-center h-screen">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
